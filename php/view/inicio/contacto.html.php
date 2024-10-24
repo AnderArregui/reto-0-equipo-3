fec8fa265ddcac5c1e353b2456266d5fd2cc2a20
Egoitz De la Llera <egoitz.delallera@ikasle.egibide.org>
2024-10-24 11:38:33 +0200

    Un comentario explicando la funcion del formulario que hay dentro de contacto

--- php/view/inicio/contacto.html.php
+++ php/view/inicio/contacto.html.php
@@ -2,6 +2,9 @@
     <div class="container">
         <h2 class="titulo">Contacto</h2>
         <div class="contacto">
+
+            /* Estamos usando un servidor para enviar formularios, cuando enviamos un formulario manda un correo a el asociado "En este caso en el de egoitz" para saber quien quiere contactar con nosotros.*/
+
             <form action="https://formsubmit.co/4c859de0c84b56f333f92b3191ccfa5b" method="POST">
                 <label for="nombre">Nombre:</label>
                 <input type="text" name="nombre" id="nombre" required>
