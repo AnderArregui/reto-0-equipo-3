f9eeec20d304e6490a66fc1f0654d8bb03b3eaa4
Egoitz De la Llera <egoitz.delallera@ikasle.egibide.org>
2024-10-24 20:47:48 +0200

    Actualizacion de varios archivos

--- php/view/usuario/perfil.html.php
+++ php/view/usuario/perfil.html.php
@@ -1,25 +1,25 @@
- <div class="general">
-
-     <?php if (isset($dataToView["data"]["usuario"])): ?>
+<div class="general">
+    <div class="container">
          <?php $usuario = $dataToView["data"]["usuario"]; ?>
+            <?php if ($usuario["tipo"] == "admin"): ?>
+                <div class="perfil">
+                    <div class="fotoPerfil">
+                        <img src="<?php echo $usuario['foto']; ?>" alt="Foto de perfil">
+                    </div>
 
-            <div class="informacion">
-               <h1 id="saludo">Hola, <?php echo htmlspecialchars($usuario['nombre']); ?> !</h1>
-               <h5 id="especialidad">Especialidad: <?php echo htmlspecialchars($usuario['especialidad']); ?></h5>
-               <h5 id="añosEmpresa">Años en la empresa: <?php echo htmlspecialchars($usuario['anios_empresa']); ?></h5>
-                <h5 id="email">Mail: <?php echo htmlspecialchars($usuario['email']); ?></h5>
-                <div class="botones">
-                   <button>Guardados</button>
-                   <button>Editar</button>
-                  <button>Cerrar cuenta</button>
-                  <button>Cambiar contraseña</button>
-             </div>
-         </div>
-     <?php else: ?>
-         <p>No se encontraron datos del usuario.</p>
-     <?php endif; ?>
-        <div class="foto">
-            <img src="usuario.png" alt="">
-        </div>
-
- </div>
+                    <h1 id="saludo">Hola, <?php echo htmlspecialchars($usuario['nombre']); ?> !</h1>
+                    <h5 id="especialidad">Especialidad: <?php echo htmlspecialchars($usuario['especialidad']); ?></h5>
+                    <h5 id="añosEmpresa">Años en la empresa: <?php echo htmlspecialchars($usuario['anios_empresa']); ?></h5>
+                        <h5 id="email">Mail: <?php echo htmlspecialchars($usuario['email']); ?></h5>
+                        <div class="botones">
+                            <button>Guardados</button>
+                            <button>Editar</button>
+                            <button>Cerrar cuenta</button>
+                            <button>Cambiar contraseña</button>
+                        </div>
+                </div>
+            
+            <?php endif; ?>
+                
+    </div>
+</div>
