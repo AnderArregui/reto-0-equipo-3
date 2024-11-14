9dbe8d87218c345a21d67628886ab7af3274b170
InigoBerastegi <161701648+InigoBerastegi@users.noreply.github.com>
2024-11-14 13:52:47 +0100

    Delete php directory

--- php/assets/js/degradado.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const temas = document.querySelectorAll('.tema');
-
-    temas.forEach(tema => {
-        const colorBase = tema.dataset.color; 
-        tema.style.setProperty('--color-base', colorBase);
-    });
-});
--- php/assets/js/guardar.js
+++
@@ -1,9 +0,0 @@
-
-function guardar(icon) {
-
-    if (icon.src.includes('nosave.png')) {
-        icon.src = '/reto-1-equipo-3/php/assets/images/save.png'; 
-    } else {
-        icon.src = '/reto-1-equipo-3/php/assets/images/nosave.png';
-    }
-}
